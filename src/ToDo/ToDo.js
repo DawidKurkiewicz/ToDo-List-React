f8b13be665e68dbefcae9b0ebd6c3555a1aa82a9
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-23 10:15:23 +0100

    added add and delete functions

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -1,13 +1,42 @@
 import React from 'react'
 
 class ToDo extends React.Component {
-    render () {
+    state = {
+        tasks: [],
+        filterText: '',
+        chosenFilter: 'All',
+        newTaskText: ''
+    }
+
+    createTask = text => ({
+        taskText: text,
+        isComplited: false,
+        key: Date.now()
+    })
+
+
+    addTask = () => this.setState({
+        tasks: this.state.tasks.concat(
+            this.createTask(
+                this.state.newTaskText
+            )
+        )
+    })
+
+deleteTask = taskKey => this.setState({
+    tasks: this.state.tasks.filter(
+        task => task.key !== taskKey
+    )
+})
+
+    render() {
         return (
             <div>
-todo
-            </div>
+                todo
+        </div>
         )
 
     }
 }
+
 export default ToDo
